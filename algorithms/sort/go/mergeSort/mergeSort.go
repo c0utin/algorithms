6f814c9cebf9254599405c7b641608721bb89707
c0utin <rafaelcouto111@gmail.com>
2024-08-13 21:41:34 -0300

    add routines because we gucci

--- algorithms/sort/go/mergeSort/mergeSort.go
+++ algorithms/sort/go/mergeSort/mergeSort.go
@@ -1,51 +1,62 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "sync"
+)
 
-func merge(left, right []int) []int{
+func merge(left, right []int) []int {
+    result := make([]int, 0, len(left)+len(right))
+    i, j := 0, 0
 
-	result := make([]int, 0, len(left)+len(right))
-
-	i,j := 0, 0;
     for i < len(left) && j < len(right) {
         if left[i] < right[j] {
             result = append(result, left[i])
             i++
-        } else { 
+        } else {
             result = append(result, right[j])
             j++
         }
     }
 
-	result = append(result, left[i:]...)
-	result = append(result, right[j:]...)
+    result = append(result, left[i:]...)
+    result = append(result, right[j:]...)
 
-	return result
-
+    return result
 }
 
-func mergeSort(arr []int) []int{
+func mergeSort(arr []int) []int {
+    if len(arr) <= 1 {
+        return arr
+    }
 
-	if len(arr) <= 1 {
-		return arr
-	}
+    mid := len(arr) / 2
 
-	mid := len(arr)/2
-	left := mergeSort(arr[:mid])
-	right := mergeSort(arr[mid:])
+    var left, right []int
 
-	return merge(left, right)
+    var wg sync.WaitGroup
+    wg.Add(2)
 
+    // Sort the left half concurrently
+    go func() {
+        defer wg.Done()
+        left = mergeSort(arr[:mid])
+    }()
+
+    // Sort the right half concurrently
+    go func() {
+        defer wg.Done()
+        right = mergeSort(arr[mid:])
+    }()
+
+    wg.Wait() // Wait for both goroutines to finish
+
+    return merge(left, right)
 }
-
-
-
-
-
 
 func main() {
-
-	arr :=  []int{1,6,3,5,9}
-	sortedArr := mergeSort(arr)
-	fmt.Printf("sorted arr: %v\n", sortedArr)
+    arr := []int{1, 6, 3, 5, 9}
+    sortedArr := mergeSort(arr)
+    fmt.Printf("Sorted arr: %v\n", sortedArr)
 }
+
