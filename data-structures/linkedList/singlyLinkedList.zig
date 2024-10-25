d0a484c7c9acaf54c690a42d857632eb245b9800
c0utin <rafaelcouto111@gmail.com>
2024-10-25 11:18:23 -0300

    [ll] initizlize struct

--- data-structures/linkedList/singlyLinkedList.zig
+++ data-structures/linkedList/singlyLinkedList.zig
@@ -1,5 +1,27 @@
 const std = @import("std");
 
+const Node = struct {
+    data: i8,
+    next: ?*Node = null,
+
+    pub fn new(allocator: *std.mem.Allocator, data: i8) !*Node {
+        const node = try allocator.create(Node);
+        node.* = Node{
+            .data = data,
+            .next = undefined,
+        };
+
+        return node;
+    }
+
+    pub fn destroy(self: *@This(), allocator: *std.mem.Allocator) void {
+        allocator.destroy(self);
+    }
+};
+
 test "Singly Linked List" {
-    std.debug.print("test", .{});
+    var allocator = std.testing.allocator;
+    const node = try Node.new(&allocator, 8);
+    std.debug.print("test {}", .{node.data});
+    node.destroy(&allocator);
 }
